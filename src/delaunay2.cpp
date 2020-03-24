72f0cfa0bc66e9a2629209fa0fd47f2f7e066efe
Keith Roberts <krober10@nd.edu>
2020-03-24 15:57:21 -0300

    working 2d del. tria

--- src/delaunay2.cpp
+++ src/delaunay2.cpp
@@ -10,19 +10,52 @@
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K> DT;
-typedef DT::Point Point;
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
 
-std::vector<double> delaunay2(std::vector<double> &x, std::vector<double> &y) 
+#include <vector>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel            Kernel;
+typedef CGAL::Triangulation_vertex_base_with_info_2<unsigned int, Kernel> Vb;
+typedef CGAL::Triangulation_data_structure_2<Vb>                       Tds;
+typedef CGAL::Delaunay_triangulation_2<Kernel, Tds>                    Delaunay;
+typedef Kernel::Point_2 Point;
+
+
+std::vector<std::vector<int>> delaunay2(std::vector<double> &x, std::vector<double> &y) 
 {
   int num_points = x.size();
   assert(y.size()!=num_points);
-  DT t;
-  for(std::size_t i = 0; i < num_points; i++) {
-     t.insert(Point(x[i],y[i]));
-    }
-  return x;
+  std::vector< std::pair<Point,unsigned> > points;
+  // add index information to form face table later
+  for(std::size_t i = 0; i < num_points; ++i) 
+  {
+     points.push_back( std::make_pair( Point(x[i],y[i]), i ) );
+  }
+
+  Delaunay triangulation;
+  triangulation.insert(points.begin(),points.end());
+
+  // save the face table 
+  int num_faces = triangulation.number_of_faces(); 
+  std::vector<std::vector<int> > faces;
+  faces.resize(3);
+  for (int i = 0; i < 3; ++i)
+    faces[i].resize(num_faces);
+
+  int i=0;
+  for(Delaunay::Finite_faces_iterator fit = triangulation.finite_faces_begin();
+    fit != triangulation.finite_faces_end(); ++fit) {
+
+    Delaunay::Face_handle face = fit;
+    faces[0][i]=face->vertex(0)->info();
+    faces[1][i]=face->vertex(1)->info();
+    faces[2][i]=face->vertex(2)->info(); 
+    //std::cout << face->vertex(0)->info() << " " << face->vertex(1)->info() << " " << face->vertex(2)->info() << std::endl;
+    i+=1;
+  }
+  return faces;
 }
 
 namespace py = pybind11;
