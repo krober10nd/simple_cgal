7f3f957d82bdde47e91ea0fdcae37d2ebac41b81
Keith Roberts <krober10@nd.edu>
2020-03-28 16:12:51 -0300

    adding in block decomposition code

---
+++ simple_cgal/blocker.py
@@ -0,0 +1,53 @@
+import numpy as np
+import math
+
+
+def blocker(points, nblocks):
+    ''' Decompose point coordinates into # of blocks (powers of 2) roughly balanced in # of points'''
+    num_points,dim = points.shape
+
+    assert nblocks%2 == 0 , 'number of blocks must be even'
+    assert nblocks >= 2, 'too few nblocks, must be >= 2'
+    assert dim > 2 or dim < 3, 'dimensions of points are wrong'
+    assert num_points//nblocks > 1, 'too few points for nblocks'
+
+    num_bisects = int(math.log2(nblocks))
+
+    block_sets = __bisect(points)
+    if num_bisects == 0 : return block_sets
+    num_bisects -= 1
+    for _ in range(num_bisects):
+        store = []
+        for block in block_sets:
+            print('bisecting')
+            tmp = __bisect(np.asarray(block))
+            store.append(tmp[0])
+            store.append(tmp[1])
+        block_sets = store
+    return block_sets
+
+
+def __bisect(points):
+    ''' Bisect point coordinates into two half spaces'''
+    num_points,dim = points.shape
+
+    x_sorted = np.argsort(points[:, 0])
+    y_sorted = np.argsort(points[:, 1])
+
+    step = num_points//2
+    ixx, iyy = np.meshgrid(
+        np.arange(num_points, step=step),
+            np.arange(num_points, step=step)
+    )
+    blocks_set = []
+    for idx, idy in zip(ixx.ravel(), iyy.ravel()):
+        common = set(x_sorted[idx: idx+step]).intersection(
+            y_sorted[idy: idy+step]
+        )
+        blocks_set.append(points.take(list(common), axis=0))
+
+    temp = []
+    for i in range(0,len(blocks_set),2):
+       temp.append(np.concatenate((blocks_set[i],blocks_set[i+1]),axis=0))
+    blocks_set = temp
+    return blocks_set
