d603cbc1891bae0cbf32037b985c71a0fc47fc13
Keith Roberts <krober10@nd.edu>
2020-03-29 12:19:50 -0300

    dealing with odd number of input points

--- simple_cgal/blocker.py
+++ simple_cgal/blocker.py
@@ -1,15 +1,21 @@
 import numpy as np
 import math
 
+def is_power_of_two(n):
+    """Return True if n is a power of two."""
+    if n <= 0:
+        return False
+    else:
+        return n & (n - 1) == 0
 
 def blocker(points, nblocks):
     ''' Decompose point coordinates into # of blocks (powers of 2) roughly balanced in # of points'''
     num_points,dim = points.shape
 
-    assert nblocks%2 == 0 , 'number of blocks must be even'
+    assert is_power_of_two(nblocks), 'nblocks must be power of 2'
     assert nblocks >= 2, 'too few nblocks, must be >= 2'
     assert dim > 2 or dim < 3, 'dimensions of points are wrong'
-    assert num_points//nblocks > 1, 'too few points for nblocks'
+    assert num_points//nblocks > 1, 'too few points for chosen nblocks'
 
     num_bisects = int(math.log2(nblocks))
 
@@ -19,7 +25,6 @@ def blocker(points, nblocks):
     for _ in range(num_bisects):
         store = []
         for block in block_sets:
-            print('bisecting')
             tmp = __bisect(np.asarray(block))
             store.append(tmp[0])
             store.append(tmp[1])
@@ -34,20 +39,20 @@ def __bisect(points):
     x_sorted = np.argsort(points[:, 0])
     y_sorted = np.argsort(points[:, 1])
 
-    step = num_points//2
+    step = num_points//2 + 1
     ixx, iyy = np.meshgrid(
         np.arange(num_points, step=step),
             np.arange(num_points, step=step)
     )
+
     blocks_set = []
     for idx, idy in zip(ixx.ravel(), iyy.ravel()):
         common = set(x_sorted[idx: idx+step]).intersection(
             y_sorted[idy: idy+step]
         )
         blocks_set.append(points.take(list(common), axis=0))
-
     temp = []
     for i in range(0,len(blocks_set),2):
-       temp.append(np.concatenate((blocks_set[i],blocks_set[i+1]),axis=0))
+        temp.append(np.concatenate((blocks_set[i],blocks_set[i+1]),axis=0))
     blocks_set = temp
     return blocks_set
