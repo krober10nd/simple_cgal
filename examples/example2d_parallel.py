5d3f1778c1cce1357d2ca9bd849d9ec56998194a
Keith Roberts <krober10@nd.edu>
2020-04-20 13:47:29 -0300

    updating example2d

--- examples/example2d_parallel.py
+++ examples/example2d_parallel.py
@@ -6,10 +6,41 @@ import matplotlib.pyplot as plt
 
 import utils
 
-num_points = 250000
-np.random.seed(0)
-gpoints = np.random.uniform(size=(num_points, 2), low=0.0, high=1.0)
-bbox = (0.0, 0.0, 1.0, 1.0)
+# structrued grid
+gpoints = [
+    [0, 0],
+    [0, 0.25],
+    [0, 0.5],
+    [0, 0.75],
+    [0, 1],
+    [0.25, 0],
+    [0.25, 0.25],
+    [0.25, 0.5],
+    [0.25, 0.75],
+    [0.25, 1],
+    [0.5, 0],
+    [0.5, 0.25],
+    [0.5, 0.5],
+    [0.5, 0.75],
+    [0.5, 1],
+    [0.75, 0],
+    [0.75, 0.25],
+    [0.75, 0.5],
+    [0.75, 0.75],
+    [0.75, 1],
+    [1, 0],
+    [1, 0.25],
+    [1, 0.5],
+    [1, 0.75],
+    [1, 1],
+]
+num_points = len(gpoints)
+# add some noise
+np.random.seed(5)
+noise = np.random.uniform(size=(num_points, 2), low=-0.01, high=0.01)
+gpoints += noise
+# triangulation to check with
+gtria = SciPyDelaunay(gpoints)
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -17,7 +48,7 @@ rank = comm.Get_rank()
 
 t1 = time.time()
 
-points, extents = utils.blocker(points=gpoints, rank=rank, nblocks=size, bbox=bbox)
+points, extents = utils.blocker(points=gpoints, rank=rank, nblocks=size)
 
 tria = SciPyDelaunay(points, incremental=True)
 
@@ -37,5 +68,11 @@ if rank == 0:
     print(upoints.shape)
     print(ufaces.shape)
     print(ufaces[:5, :])
+    fig, ax = plt.subplots()
     plt.triplot(upoints[:, 0], upoints[:, 1], ufaces)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    plt.triplot(gtria.points[:, 0], gtria.points[:, 1], gtria.vertices)
+    print(gtria.vertices.shape)
     plt.show()
